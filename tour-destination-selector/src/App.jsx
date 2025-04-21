d7d644dcb36d87593fa180cffda7f39845fdfee2
Vaclav Ruzicka <vaclavruzicka@usf.edu>
2025-04-20 22:26:51 -0400

    Filled out the App.jsx file.

---
+++ tour-destination-selector/src/App.jsx
@@ -0,0 +1,37 @@
+//App.jsx
+
+//Imports use state, TourFilter, and DestinationSelector
+import { useState } from "react"
+import TourFilter from "./components/Gallery"
+import DestinationSelector from "./components/DestinationSelector"
+
+//Creating a function that allows the website to run.
+function App() {
+  const [tours, setTours] = useState([])
+  const [selectedDestination, setSelectedDestination] = useState("")
+  
+  //Allows for a tour to be removed.
+  const onRemove = (id) => {
+    setTours((prevTours) => prevTours.filter((tour) = tour.id !== id))
+  }
+
+  //Filters the tours based on the selected destination.
+  const filteredTours = selectedDestination
+    ? tours.filter((tour) => tour.name === selectedDestination) : tours;
+
+  
+  //Calling upon destination selector and tour filter.
+  return (
+    <>
+      <h1>Tour Picker</h1>
+      <DestinationSelector
+        tours={tours}
+        onDestinationChange={setSelectedDestination}
+        />
+        <TourFilter tours={filteredTours} setTours={setTours} onRemove={onRemove} />
+      </>
+  )
+}
+
+//Exports App
+export default App
